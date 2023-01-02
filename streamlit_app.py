259c923b04c1c5ceb3ec9c3c9fb61ebdce030fe9
catiaferreirasnowflake <121810744+catiaferreirasnowflake@users.noreply.github.com>
2023-01-02 14:51:49 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 6 blueberry Oatmeal')
+streamlit.text('Kale, Spinach & r')
+streamlit.text('Omega 3 6 blueberry Oatmeal')
